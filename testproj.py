69ce796f0fc1c8d382bf5881f71af0ff929ae5df
COLE MCMASTERS <0mcmac68@solent.ac.uk>
2024-09-26 14:05:46 +0100

    testproj

--- testproj.py
+++ testproj.py
@@ -1,10 +1,22 @@
+import sys
+
 name = "Cole"
 age = 19
 
+counterAttemptMax = 4
+clientAttempts = 0
 
+while clientAttempts != counterAttemptMax:
+    answer = input("What is your name?: ")
+    if answer == "Cole":
+        print("Cole")
+        break
+    else:
+        clientAttempts += 1
+        print(clientAttempts)# this adds 1 form every new attempt
+        if clientAttempts == counterAttemptMax:
+            print("Goodbye , you have no attempts left")
+            sys.exit()
+        else:
+            print("Please try again :( ")
 
-answer = input("What is your name?: ")
-if answer == "Cole":
-    print("Cole")
-else:
-    print("Please try again :( ")
