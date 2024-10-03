4de54d72979da1ac27e9a7124a5e0a4395d4d163
COLE MCMASTERS <0mcmac68@solent.ac.uk>
2024-10-03 13:23:13 +0100

    redo logic for program 1 and 2

--- Week 2/if statements.py
+++ Week 2/if statements.py
@@ -9,28 +9,34 @@ def program1():
 
     elif bookType == "science":  # same code, just with Science!
         print(f"I like {bookType} books!!!")
-
+    else:
+        print(f"Sorry we do not have {bookType.capitalize()} in store today...")# .capitalize  makes the first letter of the variable a capital
     bookName = input("What is the name of your book? :  ")  # asks the user for the name of their book
     print(f"I love {bookName} ! That one is my favourite")  # prints out the name of their book
 
 
 
 def program2():
-    activityChoice = input("What activity do you want to do? : ").lower()
-    if activityChoice == "calculate":
-        time.sleep(.5)
-        print("\n")
-        print("doing calculations...")
-        time.sleep(.5)
-        print("Activity complete!!")
-        program2()
+    while True:
+        activityChoice = input(
+            "What activity do you want to do? : ").lower()  # this asks the user what their prefered choice is
+        if activityChoice == "calculate":  # if the user puts "calculate" then
+            time.sleep(.5)  # it runs this section of code
+            print("\n")
+            print("doing calculations...")
+            time.sleep(.5)
+            print("Activity complete!!")
+
+        else:  # if anything BUT calculate is added, it runs this
+            print("Performing activity")
+            time.sleep(.5)
+            print("Activity complete!!")
 
 
-    else:
-        print("Performing activity")
-        time.sleep(.5)
-        print("Activity complete!!")
-        program2()
 
 
+
+program1()
 program2()
+
+
