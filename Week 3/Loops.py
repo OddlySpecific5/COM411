0a08b6182de4cec7affbf21b3cf62cd68977d6ed
COLE MCMASTERS <0mcmac68@solent.ac.uk>
2024-10-10 12:58:10 +0100

    Program 1 Apple

---
+++ Week 3/Loops.py
@@ -0,0 +1,10 @@
+def program_1():
+    appleNum = int(input("How many apples do you want: "))
+    for i in range(1,appleNum + 1): # bound = "1 - appleNum "
+        print(f"{i} apples Removed...")
+
+
+
+program_1()
+
+
