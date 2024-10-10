4ca0a99da4e79174ba6f12dc732f0bb2e97bcd0c
COLE MCMASTERS <0mcmac68@solent.ac.uk>
2024-10-10 13:15:40 +0100

    Program 2

--- Week 3/Loops.py
+++ Week 3/Loops.py
@@ -1,3 +1,5 @@
+import time
+
 def program_1():
     appleNum = int(input("How many apples do you want: "))
     count = 1
@@ -6,7 +8,29 @@ def program_1():
         count += 1
 
 
+def program_2():
+    robotQuestion = int(input("How many obstecules do you want me to avoid:  "))
+    track = 0
+    while track != robotQuestion:
+        track += 1
+        print("Avoiding....")
+        print(f"Avoided {track} Obsticules!!")
+        time.sleep(.5)
+    print("\n")
+    print(f"Avoided {track} Obsticules in total..!!!!")
 
-program_1()
+
+def program_3():
+
+
+while True:#mainloop
+    print(2 * "\n")
+    question1 = int(input("What program do you want?: "))
+    if question1 == 1:
+        program_1()
+    elif question1 == 2:
+        program_2()
+    elif question1 == 3:
+        program_3()
 
 
