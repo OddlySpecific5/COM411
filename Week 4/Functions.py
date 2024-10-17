f2f4bcad244ea8af19739ef6154b47854d8be846
COLE MCMASTERS <0mcmac68@solent.ac.uk>
2024-10-17 13:30:58 +0100

    Getting the letter from Ascii

--- Week 4/Functions.py
+++ Week 4/Functions.py
@@ -2,10 +2,18 @@ import sys
 
 
 def program1():
-    print("Program Started!")
+    print(f"Program 1 Started!")
     userInput = input("Enter a character: ")
     print(f"The Ascii of your character is {ord(userInput)}")
 
+def program2():
+    print(f"Program 2 Started!")
+    userInput = int(input("Enter the Ascii code of a number: "))
+    if 32 <= userInput <= 127:
+        print(f" The Ascii code of your number is {userInput}, the character is {chr(userInput)}")
+    else:
+        print("Error")
+        print("Ending Program")
 
 
 
@@ -14,5 +22,7 @@ while True:  # mainloop
     question1 = int(input("What program do you want?: "))
     if question1 == 1:
         program1()
+    elif question1 == 2:
+        program2()
     else:
         sys.exit()
