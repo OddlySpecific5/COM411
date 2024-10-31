24909e9d404f7ff94312fcc06825510ef267ed9a
COLE MCMASTERS <0mcmac68@solent.ac.uk>
2024-10-31 13:41:18 +0000

    Display Text

--- Week 6/File handling.py
+++ Week 6/File handling.py
@@ -14,6 +14,35 @@ def run():
     t.sleep(.5)
     cwd()
 
+def display_chars(file_path, numOfLetter):
+    with open(file_path) as f:
+        print(f.read(numOfLetter).strip())
 
 
-run()
+def display_line(file_path):
+    with open(file_path) as f:
+        print(f.readline().strip())
+
+def display_text(file_path):
+    with open(file_path) as f:
+        print(f.read().strip())
+
+def run_task2():
+    print("DisplayText: " , end ="")
+    display_text(r"C:\Users\0mcmac68\PycharmProjects\COM411\files\library")
+    print()
+    print("DisplayLine: ", end="")
+    display_line(r"C:\Users\0mcmac68\PycharmProjects\COM411\files\library")
+    print()
+    print("DisplayChars: ", end="")
+    display_chars(r"C:\Users\0mcmac68\PycharmProjects\COM411\files\library", 10)
+
+
+if __name__ == "__main__":
+    question = input("What program do you want? : ")
+    if question == "1":
+        run()
+    elif question == "2":
+        run_task2()
+
+
