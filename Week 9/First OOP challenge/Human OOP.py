4d433da2ef179aeb160fa297d834173eb2aaf4cf
OddlySpecific5 <cole.mcmasters@gmail.com>
2024-11-15 14:43:45 +0000

    Added methods to display attr.

--- Week 9/First OOP challenge/Human OOP.py
+++ Week 9/First OOP challenge/Human OOP.py
@@ -8,7 +8,16 @@ class Human:
     def display_Human_name(self):
         print(f"I am {human.name}")  # prints the name
 
+    def display_Human_age(self):
+        print(f"I am {human.age}")  # prints the age
+
+    def display_Human_energy(self):
+        print(f"{human.name} has {human.energy}% left")  # prints the energy percentage
+
+
+
 
 if (__name__ == "__main__"):
     human = Human("Human", 0, 100, 100)
     human.display_Human_name()
+    human.display_Human_energy()
--- Week 9/First OOP challenge/Robot OOP.py
+++ Week 9/First OOP challenge/Robot OOP.py
@@ -8,7 +8,15 @@ class Robot:
     def display_Robot_name(self):
         print(f"I am {robot.name}")  # prints the name
 
+    def display_Robot_age(self):
+        print(f"I am {robot.age}")
+
+    def display_Human_energy(self):
+        print(f"{robot.name} has {robot.energy}% left")
+
+
 
 if (__name__ == "__main__"):
     robot = Robot("Robot", 0, 100, 100)
     robot.display_Robot_name()
+    robot.display_Human_energy()
