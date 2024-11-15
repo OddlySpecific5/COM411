685c9173fd12a4c5a80a6df7035c47c8806646a5
OddlySpecific5 <cole.mcmasters@gmail.com>
2024-11-15 15:10:32 +0000

    Updated classes human and robot with instance methods

--- Week 9/First OOP challenge/Human OOP.py
+++ Week 9/First OOP challenge/Human OOP.py
@@ -14,6 +14,27 @@ class Human:
     def display_Human_energy(self):
         print(f"{human.name} has {human.energy}% left")  # prints the energy percentage
 
+    def grow(self):
+        human.age +=1
+
+    def eat(self, amount):
+        if human.energy >= human.max_energy:# bigger or equal to just to avoid any outliers e.g. if the energy is == to 102
+            print("Energy is already full!!")
+        else:
+            human.energy += amount
+
+    def distance(self, distance):
+        human.energy -= distance# lets assume 100-102 = -2
+        if human.energy < 0:
+            tempHolder = human.energy # storing the difference value incase in need of later
+            human.energy -= human.energy # -2 - -2 = 0
+            print(f"Testing {human.name} energy {human.energy} left")
+            print(tempHolder) ## Debugging purposes
+        else:
+            print(f"You have {human.energy}% left!!")
+
+
+
 
 
 
@@ -21,3 +42,4 @@ if (__name__ == "__main__"):
     human = Human("Human", 0, 100, 100)
     human.display_Human_name()
     human.display_Human_energy()
+    human.distance(10)
--- Week 9/First OOP challenge/Robot OOP.py
+++ Week 9/First OOP challenge/Robot OOP.py
@@ -14,9 +14,29 @@ class Robot:
     def display_Human_energy(self):
         print(f"{robot.name} has {robot.energy}% left")
 
+    def grow(self):
+        robot.age +=1
 
+
+    def eat(self, amount):
+        if robot.energy >= robot.max_energy:  # bigger or equal to just to avoid any outliers e.g. if the energy is == to 102
+            print("Energy is already full!!")
+        else:
+            robot.energy += amount
+
+    def distance(self, distance):
+        robot.energy -= distance  # lets assume 100-102 = -2
+        if robot.energy < 0:
+            tempHolder = robot.energy  # storing the difference value incase in need of later
+            robot.energy -= robot.energy  # -2 - -2 = 0
+            print(f"Testing {robot.name} energy {robot.energy} left")
+            print(tempHolder)  ## Debugging
+            # logic goes here
+        else:
+            print(f"You have {robot.energy}% left!!")
 
 if (__name__ == "__main__"):
     robot = Robot("Robot", 0, 100, 100)
     robot.display_Robot_name()
     robot.display_Human_energy()
+    robot.distance(10)
